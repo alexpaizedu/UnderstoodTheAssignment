c18ecfefd768157498aece087edc6faa6bb9d37b
alexpaizedu <133619813+alexpaizedu@users.noreply.github.com>
2023-05-16 12:23:52 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -38,4 +38,46 @@ public class Main {
             System.out.println("Welcome to Understood the Assignment!");
             System.out.println("The Understood the Assignment app is a program that allows students to schedule appointments with teachers.");
  
+
+            // Get the teachers of the subject the user wants help with. Filters teachers by subject.
+            System.out.println("What subject would you like help with?");
+            String subject = input.nextLine();
+            subject = subject.toUpperCase();
+            
+            //Print the schedule of the teachers of the subject the user wants help with. Their unique schedules are retrieved via the api. 
+            TeacherArrList.printTeachersSchedule(subject);
+
+            //Get the teacher the user wants to schedule an appointment with.
+            if (TeacherArrList.printTeachersSchedule(subject) == false) {
+                //Error message
+                System.out.println("Sorry, there are no teachers available for that subject.");
+                //Prompt user to try again
+                System.out.println("Would you like to try again? (Y/N)");
+                String answer = input.nextLine();
+                answer = answer.toUpperCase();
+                if (answer.equals("N")) {
+                    break;
+
+                }
+            } 
+            else {
+                System.out.println("Which teacher would you like to schedule an appointment with? (Please copy and paste the exact name that appears)");
+                String teacherName = input.nextLine();
+                Teacher tutor = TeacherArrList.getTeacher(teacherName);
+     
+                tutor.bookMeeting();
+                //The user is given a link to book a meeting via Calendly, and the link is retrieved via the api. By default, only bookings in which the teacher is avaliable are avaliable. 
+                //After booking, a confirmation email is sent to both parties (Calendly takes care of that). 
+                System.out.println("Once you have follwed through the Calendly link, your appointment has been booked!");
+                System.out.println("Would you like to schedule another appointment? (Y/N)");
+                String answer = input.nextLine();
+                answer = answer.toUpperCase();
+                if (answer.equals("N")) {
+                    status = false;
+                }
+            }
+        }
+        System.out.println("Thank you for booking with Understood the Assignment!");
+    }
+    
 }
