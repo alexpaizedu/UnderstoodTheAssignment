c54488fbec9c51385ba2510927c31df817b7d9cb
alexpaizedu <133619813+alexpaizedu@users.noreply.github.com>
2023-05-16 12:44:07 -0600

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,28 +1,38 @@
+//View ReadMe file for references. Microsoft CoPilot free trial aided in suggesting the necessary imports. The org.json imports were done by installing the .jar file.
+
+
+//These prompts are to make calls to the api through the endpoint in order to recieve a response. The api is REST based.
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.ArrayList;
+
+//These improts are to parse the JSON api response. The api is REST based.
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 public class Teacher {
     private String name;
     private String subject;
     private String apiKey;
-    private List<AvailableBlock> availableTimes; 
+    //The teacher has an arraylist of type AvailableBlock to display their avaliability schedule.
+    private arrayList<AvailableBlock> availableTimes; 
 
+    
+    //Constructor
     public Teacher(String name, String subject, String apiKey) {
         this.name = name;
         this.subject = subject;
         this.apiKey = apiKey;
         this.availableTimes = new ArrayList<>();
     }
-
+    
+    //Getter methods
     public String getName() {
         return name;
     }
@@ -31,11 +41,16 @@ public class Teacher {
         return subject;
     }
     
+    
+    //This works with the api's endpoint to get each teacher's unqiue scheduling link.
     private String getUserSchedulingLink() {
         String apiUrl = "https://api.calendly.com/users/me";
-
+        //Learned about try and catch with some of the videos in the ReadMe file.
+        //Calendly API has great documentation to work with the api.
         try {
+            //Makes request to the API with parameters Authorization, Bearer (instead of OAUTH), and the user's API key.
             URL url = new URL(apiUrl);
+            //Makes the call to the endpoint.
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Content-Type", "application/json");
@@ -50,14 +65,17 @@ public class Teacher {
                     responseBuilder.append(line);
                 }
                 reader.close();
-
+                
+                
                 String response = responseBuilder.toString();
                 JSONObject responseObject = new JSONObject(response);
                 JSONObject resourceObject = responseObject.getJSONObject("resource");
-
+                
+                //Returns the scheduling_url only from the API response.
                 String userSchedulingLink = resourceObject.getString("scheduling_url");
                 return userSchedulingLink;
-            } else {
+            } else 
+            {
                 System.out.println("API request failed. Response Code: " + responseCode);
             }
         } catch (IOException | JSONException e) {
@@ -67,11 +85,13 @@ public class Teacher {
         return null;
     }
 
+     //Prompts user tobook meeting by retrieving scheduling link.
     public void bookMeeting() {
         System.out.println("Book meeting through this link");
         System.out.println(getUserSchedulingLink());
     }
 
+    //This is an API call identical to get scheduling url, but instead retrieving the user URI. We need the user URI because it is one of the parameters for another api call to get the avaliability schedules.
     private String getUserUri() {
         String apiUrl = "https://api.calendly.com/users/me";
 
@@ -107,7 +127,10 @@ public class Teacher {
 
         return null;
     }
-
+    
+    
+    //This code parses the JSON response and puts in into each teacher's avalibility schedule array. The teacher's array are of object AvailableBlock. 
+    // For this code segment, Microsoft Copilot aided in making the arrays from the JSON responses because I did not now how to parse JSON.
     private void parseAvailabilityResponse(String response) {
         availableTimes.clear();
 
@@ -138,7 +161,9 @@ public class Teacher {
             e.printStackTrace();
         }
     }
-
+    
+    
+    //This method is the API response for avaliability schedules that gets used in the method above. The value returned from this API call is what is parsed and put into the teacher's arraylist.
     private String getApiResponse(String apiUrl) {
         try {
             URL url = new URL(apiUrl);
@@ -167,7 +192,9 @@ public class Teacher {
 
         return null;
     }
-
+    
+    // This method uses the previous method getUserUri() because it is a necessary parameter to retrieve the tecaher's avaliability schedule.
+    //This method prints out the teachers' avaliable times neatly so that students can choose a teacher with a schedule convinient for him/her.
     public void getAvailability() {
         String userUri = getUserUri();
 
