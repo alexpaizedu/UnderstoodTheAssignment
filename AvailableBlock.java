d46c2613053ea0a146a30707ead10bdec8306ce2
alexpaizedu <133619813+alexpaizedu@users.noreply.github.com>
2023-05-16 12:10:26 -0600

    Update AvailableBlock.java
    Commented code

--- AvailableBlock.java
+++ AvailableBlock.java
@@ -1,14 +1,22 @@
+//this class contains the object that will be stored in each of the teachers' array list of avaliable schedules. The data parsed from json will be stored as objects of the teacer's array list.
+
+
 public class AvailableBlock {
+    
+    //instance variables
     private String date;
     private String from;
     private String to;
 
+    
+    //constructor
     public AvailableBlock(String date, String from, String to) {
         this.date = date;
         this.from = from;
         this.to = to;
     }
-
+    
+    //getter methods
     public String getDate() {
         return date;
     }
